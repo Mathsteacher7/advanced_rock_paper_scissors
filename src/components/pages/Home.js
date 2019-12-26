026577fad174764eff03c95b4af28283e2ab8da7
Daniel H Cohen <hofcohen@gmail.com>
2019-12-26 18:46:17 +0000

    spelling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="home">
             <h1>Let's play a game!</h1>
-            <p>This website is for you, so you will never be board. I will add here games over time, so enjoy it!</p>
+            <p>This website is for you, so you will never be bored. I will add here games over time, so enjoy it!</p>
         </div>
 
     )
