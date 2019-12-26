2f0a2f0bd058719a3996615a9a3b78439097a0ab
Daniel H Cohen <hofcohen@gmail.com>
2019-12-26 18:38:43 +0000

    starting to work on tictactoe have a grid

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            <p>Photos by Zoltan Tasi, Morgan Harris, Stefan Cosma, Olivia Henry, Nazish Mirekar and Skyla Design on Unsplash</p>
+            <p>Photos by Zoltan Tasi, Morgan Harris, Stefan Cosma, Olivia Henry, Carl Raw, Nazish Mirekar and Skyla Design on Unsplash</p>
             <p>Created by <a href="https://github.com/Mathsteacher7" target="_blank"> Daniel H Cohen</a>, you can find other projects of mine <a href="https://mathsteacher7.github.io/" target="_blank">here</a></p>
         </footer>
 
--- src/components/games/TicTacToe.js
+++ src/components/games/TicTacToe.js
@@ -1,10 +1,38 @@
 import React from 'react'
 
+// let grid = []
+// for (let i = 0; i < 9; i++){
+//     grid.push(i)
+// }
+// console.log(grid)
 
 class TicTacToe extends React.Component {
+
+    constructor(){
+        super()
+
+
+    }
+
+
+    changeBackground(e){
+        console.log(e)
+
+    }
+
     render(){
+        let grid = []
+        for (let i = 0; i < 9; i++){
+            grid.push(i)
+        }
         return(
-            <h1 className="title">Soon we will have here also TicTacToe</h1>
+            <div className="tictactoe">
+
+                <h1 className="title">Soon we will have here also TicTacToe</h1>
+                <div className="boxes">
+                    {grid.map(box => <button onClick={this.changeBackground} key={box} className="empty ">{box}</button>)}
+                </div>
+            </div>
         )
     }
 }
